9d7de52dd68dc19cd502cf6db3a4784b0dcfcf20
pengyu guo <psypg2@nottingham.ac.uk>
2020-01-20 16:39:04 +0000

    tet
    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,12 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("yes")) {
+            return "恭喜你意识到了这一点";
+        }
+        if (query.toLowerCase().contains("no")) {
+            return "别挣扎了，你是";
+        }
         return "";
     }
 }
--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
-                "<p> Is liping sha zi?" +
+                "<p> Is liping 傻子?" +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
