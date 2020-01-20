8ac00690751d1525b0e457b558b8dadac1a07df1
pengyu guo <psypg2@nottingham.ac.uk>
2020-01-20 16:21:39 +0000

    make it better

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
-                "<p>your query in the box below: " +
+                "<p> Is liping sha zi?" +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
